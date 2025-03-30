d56676c67d425c9cbc91c31ae6efabf776daced0
Vishal vignesh <pokemongo123vishal@gmail.com>
2025-03-31 01:05:59 +0530

    parser problem removed

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ if (!MONGO_URI) {
   console.log("🔍 Trying to connect to MongoDB...");
   
   mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
     useUnifiedTopology: true,
   })
     .then(() => console.log("✅ Connected to MongoDB! 🚀"))
