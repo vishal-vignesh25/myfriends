cb591b8936c334d83dbfdf4faca5cc2f9ac72c99
Vishal vignesh <pokemongo123vishal@gmail.com>
2025-03-31 01:24:26 +0530

    changes done

--- server.js
+++ server.js
@@ -52,7 +52,6 @@ app.post("/upload", upload.single("image"), async (req, res) => {
     try {
         if (!req.file) return res.status(400).json({ success: false, message: "No image uploaded" });
 
-        // Upload to Cloudinary
         const result = await new Promise((resolve, reject) => {
             const uploadStream = cloudinary.uploader.upload_stream({ resource_type: "image" }, (error, result) => {
                 if (error) reject(error);
@@ -61,7 +60,6 @@ app.post("/upload", upload.single("image"), async (req, res) => {
             uploadStream.end(req.file.buffer);
         });
 
-        // Save image link to MongoDB
         const newImage = new Image({ url: result.secure_url });
         await newImage.save();
 
